1e1aae48330e523f17a8e67ff238bd534e1bed14
Youngsuh Chae <anica0206@gmail.com>
2023-12-12 19:39:25 +0800

    login, signup, logout

--- src/main/java/com/example/demo12/controller/HomeController.java
+++ src/main/java/com/example/demo12/controller/HomeController.java
@@ -1,28 +1,32 @@
 package com.example.demo12.controller;
 
-import com.example.demo12.mapper.HomeMapper;
 import com.example.demo12.mapper.chartMapper;
+import com.example.demo12.model.UserVO;
 import com.example.demo12.model.chartMainVO;
+import com.example.demo12.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 
+import javax.servlet.http.HttpSession;
 import java.util.List;
-import java.util.Map;
 
 @Controller
 public class HomeController {
 
     @Autowired
-    private HomeMapper homeMapper;
+    private UserService userService;
 
-    @RequestMapping("/")
-    public String home() {
-        System.out.println("-----");
-        List<Map<String, String>> list = homeMapper.getTime();
-        System.out.println("-----");
-        return "home";
+    @GetMapping("/")
+    public String home(HttpSession session, Model model) {
+        String id = (String) session.getAttribute("userId");
+        if (id != null) { // 로그인된 상태
+            UserVO userVO = userService.getUserById(id);
+            model.addAttribute("user", userVO);
+            return "home";
+        }
+        return "redirect:/login";
     }
 
     public class chartMainController {
