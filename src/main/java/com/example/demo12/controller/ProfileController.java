27763d2a29a3ecc77c7b61ad3188a6bd14e1d301
heosungmu <sungmu1193@gmail.com>
2023-12-14 18:00:44 +0900

    Changes ProfileController Prifile

---
+++ src/main/java/com/example/demo12/controller/ProfileController.java
@@ -0,0 +1,27 @@
+package com.example.demo12.controller;
+
+import com.example.demo12.model.UserVO;
+import com.example.demo12.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import javax.servlet.http.HttpSession;
+
+@Controller
+public class ProfileController {
+    @Autowired
+    private UserService userService;
+
+    @GetMapping("/Profile")
+    public String Profile(HttpSession session, Model model) {
+        String id = (String) session.getAttribute("userId");
+        if (id != null) { // 로그인된 상태
+            UserVO userVO = userService.getUserById(id);
+            model.addAttribute("user", userVO);
+            return "Profile";
+        }
+        return "redirect:/login";
+    }
+}
---
+++ src/main/java/com/example/demo12/model/Profile.java
@@ -0,0 +1,7 @@
+package com.example.demo12.model;
+
+public class Profile {
+    private String user_id;
+    private String d_job;
+    private String education;
+}
