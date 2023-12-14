0327a514b063de85affd88e48593d9f676d2f582
Youngsuh Chae <anica0206@gmail.com>
2023-12-14 09:11:28 +0800

    BoardService 추가

---
+++ src/main/java/com/example/demo12/service/BoardService.java
@@ -0,0 +1,38 @@
+package com.example.demo12.service;
+
+import com.example.demo12.mapper.BoardMapper;
+import com.example.demo12.model.Board;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+
+@Service
+public class BoardService {
+
+    @Autowired
+    private BoardMapper boardMapper;
+
+    public Board getBoardByBoardNo(int boardNo) {
+        return boardMapper.getBoardByBoardNo(boardNo);
+    }
+
+    public void addReferenceNo(Board board) {
+        boardMapper.updateBoardReferenceNo(board);
+    }
+
+    public List<Board> getBoardList() {
+        return boardMapper.getBoards();
+    }
+
+    public void addBoard(Board board){
+        boardMapper.insertBoard(board);
+    }
+    public void modifyBoard(Board board){
+        boardMapper.updateBoardByBoardId(board);
+    }
+    public void deleteBoard(Board board){
+        boardMapper.deleteBoardByBoardId(board);
+    }
+}
